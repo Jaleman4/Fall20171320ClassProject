0ef62618313f356a8b81162d96750cc1f38ce606
Jaleman4 <jose.aleman2013@gmail.com>
2017-11-30 13:47:05 -0600

    This is my initial project

---
+++ GitHubExample/GitHubExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is my Hello World Console Application 
+            Console.WriteLine("Hello world ");
+            Console.ReadKey();
+
+        }
+    }
+}
